dd3b140f3e881f2771d663fbbf30b3e45c5aff51
ychoy000 <>
2025-06-24 18:02:17 +0900

    0624

---
+++ 70_pf/tourist/js/slider.js
@@ -0,0 +1,16 @@
+// JavaScript
+
+$(function () {
+  $(".bxslider").bxSlider({
+    // 'horizontal', 'vertical', 'fade'
+    mode: "fade",
+    auto: true,
+    // play/pause - 실행/일시정지
+    // autoControls: true
+    stopAutoOnClick: true,
+    // pagenation(indicator) 
+    // -> 하단 중앙의 동그라미 버튼
+    // pager: false,
+    slideWidth: 1903,
+  });
+});
