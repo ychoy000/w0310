984177e4e1b6c8126c6acf07932caf9175001d42
ychoy000 <>
2025-07-08 18:29:10 +0900

    0708

--- 70_pf/srt/script/routemap.js
+++ 70_pf/srt/script/routemap.js
@@ -1,22 +1,21 @@
     
+// 노선안내 서브페이지 노선별 지도 바꾸기
+   
+const tabs = document.querySelectorAll('.tab');
+const images = document.querySelectorAll('.map_container img');
 
-    const tabs = document.querySelectorAll('.tab');
-    const images = document.querySelectorAll('.map_container img');
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const index = parseInt(tab.getAttribute('data-index'), 10); // Convert to number
 
-    console.log(tabs);
-    console.log(images);
-    
-    tabs.forEach(tab => {
-      tab.addEventListener('click', () => {
-        const index = tab.getAttribute('data-index');
+    // 탭 활성화 전환
+    tabs.forEach(t => t.classList.remove('active'));
+    tab.classList.add('active');
 
-        // 탭 활성화 전환
-        tabs.forEach(t => t.classList.remove('active'));
-        tab.classList.add('active');
+    // 이미지 표시 전환
+    images.forEach(img => img.classList.remove('active'));
+    images[index].classList.add('active');
+  });
+});
 
-        // 이미지 표시 전환
-        images.forEach(img => img.classList.remove('active'));
-        images[index].classList.add('active');
-      });
-    });
 
