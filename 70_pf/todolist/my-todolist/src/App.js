404e511dd0e10881738a1466d8817e9ae1d89fe3
ychoy000 <>
2025-08-08 15:10:11 +0900

    0808

---
+++ 70_pf/todolist/my-todolist/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1><i className="fa-solid fa-list-ul text-primary me-3"></i>To Do List</h1>
+      <input type="text" />
+      <button>추가</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ 70_pf/todolist/my-todolist/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";  // Bootstrap CSS
+import "@fortawesome/fontawesome-free/css/all.min.css"; // Font Awesome
+import './css/index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
