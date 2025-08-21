42a85f3061b9e888afcd618482ca7b7116852255
ychoy000 <>
2025-08-21 18:01:28 +0900

    0821

--- 70_pf/moviespace-react/src/App.js
+++ 70_pf/moviespace-react/src/App.js
@@ -1,45 +1,80 @@
 import "./App.css";
-import { HashRouter, Route, Routes, NavLink } from "react-router-dom";
-
+import { useState } from 'react';
+import logo from "./logo.png";
 
 function Movies() {
+  const movies = [
+    {
+      image: "https://img.megabox.co.kr/SharedImg/2025/07/21/PyQHev3OAVz7SGjIbnc6hGIA7GHmqq8g_420.jpg",
+      alt: "m4",
+      title: '악마가 이사왔다.',
+      issue: '빠져든다. 8월 13일 극장 대개봉',
+    },
+    {
+      image: "https://img.megabox.co.kr/SharedImg/2025/07/25/xW9c9qjuflwzzoHf6PPnfWOE5cAF4v4j_420.jpg",
+      alt: "m1",
+      title: '머티리얼리스트',
+      issue: '사랑과 결혼 앞에선 누구나 속물이 된다.',
+    },
+    {
+      image: "https://img.megabox.co.kr/SharedImg/2025/07/25/4fLIzM7kJAnaPCu4ngQJLhgP7ak3GMrs_420.jpg",
+      title: "귀멸의 칼날",
+      issue: '무한성, 8월 22일 대개봉',
+    },
+    {
+      image: "https://img.megabox.co.kr/SharedImg/2025/08/05/GCFwvyXNbZTEogfoCW4TQNLiBNc5l6Hy_420.jpg",
+      title: "좀비딸",
+      issue: '우리 애는 안 울어요',
+    }
+  ];
+
   return (
-    <div>
-      <h3>Movies</h3>
-      Movies ...
+    <div className="boxoffice">
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.rank}
+          rank={movie.rank}
+          image={movie.image}
+          title={movie.title}
+          issue={movie.issue}
+        />
+      ))}
     </div>
   );
 }
-function Series() {
+
+function MovieCard({image, title, issue }) {
   return (
-    <div>
-      <h3>Series</h3>
-      Series ...
-    </div>
-  );
-}
-function Contact() {
-  return (
-    <div>
-      <h3>Contact</h3>
-      Contact ...
+    <div className="movie-card">
+      <img src={image} alt="img" />
+      <div class="movie-info">
+        <h4>{title}</h4>
+        <p className="issue">{issue}</p>
+      </div>
     </div>
   );
 }
 
 function App() {
-  return (
-    <div className="app">
-      <div className="logo">
-        <img src='ico-img-none.png' alt='film' />
-        <span className="tlogo"> Movie Space</span>
+   return (
+    <div>
+      <div className="header">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <span className="tlogo"> Movie Space</span>
+        </div>
+        <ul className="gnb">
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">Series</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
       </div>
 
-      <ul className="gnb">
-        <li><NavLink to="/">Movies</NavLink></li>
-        <li><NavLink to="/series">Series</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
-      </ul>
+      <Movies />
+
+      <div className="footer">
+        <p>ⓒ2025 MovieSpace. All rights reserved.</p>
+      </div>
     </div>
   );
 }
